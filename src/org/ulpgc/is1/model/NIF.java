87682eb081ae50722b33807076fc173ce65725f5
Saul <saulpablofg2@gmail.com>
2023-10-13 12:04:09 +0100

    Se creo la clase nif falta crear el resto de clases

---
+++ src/org/ulpgc/is1/model/NIF.java
@@ -0,0 +1,18 @@
+package org.ulpgc.is1.model;
+#Crear resto de calses
+public class NIF {
+    public String number;
+
+    public NIF(String number) {
+        this.number = number;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+}
