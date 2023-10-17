bfc587f1f4cfdefd852355890c696bae7288ebf2
Saul <saulpablofg2@gmail.com>
2023-10-17 19:09:56 +0100

    Falta corregir y terminar el PaddelManager,codificar el Main y comprobar su funcionamiento

---
+++ src/org/ulpgc/is1/model/PaddleManager.java
@@ -0,0 +1,13 @@
+package org.ulpgc.is1.model;
+
+public class PaddleManager extends Customer{
+    public PaddleManager(String name, String surname) {
+        super(name, surname);
+    }
+
+    public void addCustomer(String name, String Surname){
+
+    }
+
+
+}
