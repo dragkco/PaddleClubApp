3ba05faf16c8eb836f4d71d4428a05d7df6c14f5
TheFox50011 <148113970+TheFox50011@users.noreply.github.com>
2023-10-19 09:31:27 +0100

    Mejoras a la clase Member
    Se ha implementado un set Adress y se la ha extendidod la clase Member a la classe Customer

--- src/org/ulpgc/is1/model/Member.java
+++ src/org/ulpgc/is1/model/Member.java
@@ -1,10 +1,12 @@
 package org.ulpgc.is1.model;
 
-public class Member {
+public class Member extends Customer{
     public int points;
 
-    public Member(int points) {
+    public Member(int points,String Name,String Surname,String Street,String city,String,int number,int postalCode) {
+        super(Name,Surname);
         this.points = points;
+        setAdresss(Street,city,postalCode,number)
     }
 
     public int getPoints() {
